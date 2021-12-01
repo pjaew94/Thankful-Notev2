8cf9b42ebc7b4566bfeccb83bf77f31f0ad02f06
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2021-12-01 13:36:05 -0500

    head added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import React from "react";
+import Head from "next/head";
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return   <React.StrictMode>
+     <Head>
+            <title>Thankful Notes</title>
+            <link rel="shortcut icon" href="/groupImg.png" />
+          </Head>
+     <Component {...pageProps} /></React.StrictMode>
 }
 
 export default MyApp
