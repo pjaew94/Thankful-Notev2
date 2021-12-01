e779de4e7ddf366689e9b141c1c2d700a76f165a
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2021-12-01 03:04:11 -0500

    date and time fix

--- helpers/helperFunc.ts
+++ helpers/helperFunc.ts
@@ -1,45 +1,29 @@
 const checkToday = (date: string) => {
   const now = new Date();
   
-  // const last = new Date(date);
-  // last.setHours(last.getHours() - 5);
-
-
-  const nowDate = now.getDate();
-  const nowMonth = now.getMonth(); //It comes in respect to index(starts at 0);
-  const nowYear = now.getFullYear();
-  const nowHour = now.getHours();
-  const nowMin = now.getMinutes();
-
   const last = new Date(date);
+  last.setHours(last.getHours() - 5);
+
+
   const lastDate = last.getDate();
-  const lastMonth = last.getMonth(); //It comes in respect to index(starts at 0);
-  const lastYear = last.getFullYear();
-  const lastHour = last.getHours();
-  const lastMin = last.getMinutes();
+  const nowDate = now.getDate();
 
-  const MS_PER_DAY = 1000 * 60 * 60 * 24;
+  console.log(lastDate)
+  console.log(nowDate)
 
-  const nowUTC = Date.UTC(nowYear, nowMonth, nowDate, nowHour, nowMin);
-  const lastPostUTC = Date.UTC(
-    lastYear,
-    lastMonth,
-    lastDate,
-    lastHour,
-    lastMin
-  );
-
-  const diff = (nowUTC - lastPostUTC) / MS_PER_DAY;
-
-  if (diff > 1) {
-    return false;
+  if(lastDate !== nowDate){
+    return false
   } else {
-    return true;
+    return true
   }
+
+
+
 };
 
 const convertDate = (date: string) => {
   const toBeConverted = new Date(date) 
+  toBeConverted.setHours(toBeConverted.getHours() - 5);
   const dateMap: {
     [key: number]: string;
   } = {
